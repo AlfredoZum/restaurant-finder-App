c52cfc9c2b948c469fa02d2fd2d99e57270827dd
AlfredoZum <alfredo.conomia@gmail.com>
2021-06-29 00:32:08 -0500

    add list restaurant in offers

---
+++ src/scenes/offers/components/list-restaurant.jsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  Image,
+} from 'react-native';
+
+import { Entypo } from '@expo/vector-icons';
+import { primaryColor, grayColor } from '../../../utils/colors';
+
+const styles = StyleSheet.create({
+  scrollView: {
+    // marginBottom: 275,
+    marginTop: 8,
+    marginBottom: 20,
+  },
+  restaurantContainer: {
+    marginLeft: 16.0,
+    marginRight: 16.0,
+    marginTop: 16.0,
+  },
+  restaurantBanner: {
+    width: '100%',
+    height: 200.0,
+  },
+  containerInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  nameRestaurant: {
+    marginTop: 8.0,
+    marginBottom: 8.0,
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  scoreRestaurant: {
+    color: primaryColor,
+    fontSize: 12,
+    marginRight: 4,
+  },
+  textGeneralRestaurant: {
+    fontSize: 12,
+    color: grayColor,
+    marginRight: 4,
+    fontWeight: '500',
+  },
+  textpriceRestaurant: {
+    fontSize: 14,
+    color: grayColor,
+    marginLeft: 4,
+    fontWeight: '500',
+  },
+  offersDiv: {
+    position: 'absolute',
+    backgroundColor: primaryColor,
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+    bottom: 10,
+    right: 10,
+    borderRadius: 20,
+  },
+  offersText: {
+    color: '#FFFFFF',
+    fontSize: 14,
+  },
+});
+
+function ListRestaurant(props) {
+  const prop = props;
+  const { restaurants } = prop;
+
+  const indexText = (index) => index;
+
+  return (
+    <ScrollView style={styles.scrollView} showsHorizontalScrollIndicator={false}>
+      {restaurants.map((restaurant, index) => (
+        <Restaurant key={indexText(index)} restaurant={restaurant} />
+      ))}
+    </ScrollView>
+  );
+}
+
+function Restaurant(props) {
+  const prop = props;
+  const { restaurant } = prop;
+
+  const ratings = `(${restaurant.ratings} ratings)`;
+  const price = `· ${restaurant.price}`;
+
+  return (
+    <View style={styles.restaurantContainer}>
+      <View style={styles.restaurantContainerImage}>
+        <Image style={styles.restaurantBanner} source={restaurant.banner} />
+        <View style={styles.offersDiv}>
+          <Text style={styles.offersText}>{restaurant.offers}</Text>
+        </View>
+      </View>
+      <Text style={styles.nameRestaurant}>{restaurant.name}</Text>
+      <View style={styles.containerInfo}>
+        <Entypo
+          style={styles.iconStart}
+          name="star"
+          size={16}
+          color={primaryColor}
+        />
+        <Text style={styles.scoreRestaurant}>{restaurant.score}</Text>
+        <Text style={styles.textGeneralRestaurant}>{ratings}</Text>
+        <Text style={styles.textGeneralRestaurant}>{restaurant.type}</Text>
+        <Text style={styles.textpriceRestaurant}>{price}</Text>
+      </View>
+    </View>
+  );
+}
+
+export default ListRestaurant;
--- src/scenes/offers/containers/offers.jsx
+++ src/scenes/offers/containers/offers.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { primaryColor } from '../../../utils/colors';
 
 import Header from '../components/header';
+import ListRestaurant from '../components/list-restaurant';
+
+import {
+  restaurant1,
+  restaurant2,
+  restaurant3,
+  restaurant4,
+} from '../../../utils/imagesRestaurant';
 
 const styles = StyleSheet.create({
   container: {
@@ -27,11 +35,54 @@ const styles = StyleSheet.create({
 });
 
 function Offers() {
+  const restaurants = [
+    {
+      id: 1,
+      name: 'Loba Restaurante',
+      score: 4.7,
+      ratings: 124,
+      type: 'cocina y cerveza en la fábrica',
+      price: '$$',
+      banner: restaurant1,
+      offers: 'free shipping',
+    },
+    {
+      id: 2,
+      name: 'Quelite',
+      score: 4.7,
+      ratings: 124,
+      type: 'Mexicana, Contemporánea',
+      price: '$$',
+      banner: restaurant2,
+      offers: 'free shipping',
+    },
+    {
+      id: 3,
+      name: 'Mononoke Ramen',
+      score: 4.7,
+      ratings: 124,
+      type: 'Restaurante ramen',
+      price: '$$',
+      banner: restaurant3,
+      offers: 'free shipping',
+    },
+    {
+      id: 4,
+      name: 'Caballo De Mar',
+      score: 4.7,
+      ratings: 124,
+      type: 'Mariscos ',
+      price: '$$',
+      banner: restaurant4,
+      offers: 'free shipping',
+    },
+  ];
+
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.body}>
-        <Text>Que onda loco</Text>
+        <ListRestaurant restaurants={restaurants} />
       </View>
     </View>
   );
